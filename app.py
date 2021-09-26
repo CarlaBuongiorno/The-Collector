54b33d25ad7e3169563da3a94fdd84ca6df157be
Carla Buongiorno <carlab201103@gmail.com>
2021-09-26 05:32:57 +0000

    Add Register template

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import (
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -22,6 +23,11 @@ mongo = PyMongo(app)
 def get_comics():
     comics = mongo.db.comics.find()
     return render_template("comics.html", comics=comics)
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    return render_template("register.html")
 
 
 if __name__ == "__main__":
