a11fa3aed1674be0231d14965771b55195aa8f92
Carla Buongiorno <carlab201103@gmail.com>
2021-10-01 11:41:31 +0000

    Add Session User Profile

--- app.py
+++ app.py
@@ -59,6 +59,8 @@ def register():
         # put the new user into 'session' cookie
         session["user"] = request.form.get("username").lower()
         flash("Registration Successful!")
+        return redirect(url_for("profile", username=session["user"]))
+
     return render_template("register.html")
 
 
@@ -74,7 +76,10 @@ def login():
             if check_password_hash(
                 existing_user["password"], request.form.get("password")):
                 session["user"] = request.form.get("username").lower()
-                flash("Welcome, {}".format(request.form.get("username")))
+                flash("Welcome, {}".format(
+                    request.form.get("username")))
+                return redirect(url_for(
+                    "profile", username=session["user"]))
             else:
                 # invalid password match
                 flash("Incorrect Username and/or Password")
@@ -86,6 +91,14 @@ def login():
             return redirect(url_for("login"))
 
     return render_template("login.html")
+
+
+@app.route("/profile/<username>", methods=["GET", "POST"])
+def profile(username):
+    # grab the session user's username from the db
+    username = mongo.db.user.find_one(
+        {"username": session["user"]})["username"].title()
+    return render_template("profile.html", username=username)
 
 
 if __name__ == "__main__":
