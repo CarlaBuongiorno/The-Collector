4e8c1d6430e7034f5cd3f36c90b9b0c4e5d96d02
Carla Buongiorno <carlab201103@gmail.com>
2021-10-03 21:17:31 +0000

    Create add_comic template

--- app.py
+++ app.py
@@ -113,6 +113,11 @@ def logout():
     return redirect(url_for("login"))
 
 
+@app.route("/add_comic")
+def add_comic():
+    return render_template("add_comic.html")
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
