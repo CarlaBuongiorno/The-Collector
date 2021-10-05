bd4969f666290adf434976df6b90eb7c37dd45b1
Carla Buongiorno <carlab201103@gmail.com>
2021-10-05 11:57:52 +0000

    Move modal javascript from script.js into comics.html block scripts

--- static/js/script.js
+++ static/js/script.js
@@ -1,8 +1,1 @@
-// Modal
-// credit -> https://getbootstrap.com/docs/4.0/components/modal/
-var myModal = document.getElementById('myModal')
-var myInput = document.getElementById('myInput')
 
-myModal.addEventListener('shown.bs.modal', function () {
-  myInput.focus()
-})
