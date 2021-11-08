4af1331bb40878f2530e7dfb437f3bcc5f6d096f
Carla Buongiorno <carlab201103@gmail.com>
2021-11-08 14:45:25 +0000

    remove home.js

--- static/js/home.js
+++
@@ -1,9 +0,0 @@
-function displayInfo(event) {
-    console.log("Hello");
-
-    let catalogueComics = document.getElementById("catalogue-comics");
-    let browseComics = document.getElementById("browse-comics");
-    let buyAndSell = document.getElementById("buy-&-sell");
-
-    catalogueComics.addEventListener("click", displayInfo);
-}
